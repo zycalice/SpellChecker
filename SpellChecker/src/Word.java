1e01b6a51602a43c309dd878886df4db29797693
zycalice <alicezhang727@gmail.com>
2020-10-26 17:24:22 -0400

    edited class Word to include right string array

--- SpellChecker/src/Word.java
+++ SpellChecker/src/Word.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Word {
     String word;
-    String[] chars;
+    String[] leftChars;
+    String[] rightChars;
     int numChars;
 
     /**
@@ -11,15 +11,39 @@ public class Word {
      * @param wordInput, a word in string format
      */
     Word(String wordInput){
+        //save word, array of characters, and number of characters
         word = wordInput;
-        chars = wordInput.split("");
-        numChars = chars.length;
+        leftChars = wordInput.split("");
+        numChars = leftChars.length;
     }
+
+    /**
+     * get an string array for each character from the right (reverse ordered)
+     * @return string array for each character from the right (reverse ordered)
+     */
+    String[] getRightChars(){
+        rightChars = new String[numChars];
+
+        for (int i = 0; i < numChars; i++){
+            rightChars[i] = leftChars[numChars-i-1];
+        }
+        return rightChars;
+    }
+
+    /**
+      get an string array for each character from the left
+     * @return string array for each character from the left
+     */
+    String [] getLeftChars(){
+        return leftChars;
+    }
+
 
     public static void main(String[] args) {
         Word w = new Word("vqqhahaha");
-        System.out.println(Arrays.toString(w.chars));
-        System.out.println(w.chars[0]);
-        System.out.println(w.chars[w.numChars-1]);
+        System.out.println(Arrays.toString(w.getLeftChars()));
+        System.out.println(Arrays.toString(w.getRightChars()));
+        System.out.println(w.getLeftChars()[0]);
+        System.out.println(w.getRightChars()[0]);
     }
 }
