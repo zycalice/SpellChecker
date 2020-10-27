ee97530b693436dde6983533f7fca51bb9828daf
zycalice <alicezhang727@gmail.com>
2020-10-27 15:43:50 -0400

    updated relative path in ReadWrite

--- SpellChecker/src/ReadWrite.java
+++ SpellChecker/src/ReadWrite.java
@@ -2,8 +2,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.nio.file.*;
 
 public class ReadWrite {
+
+    Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
 
     /**
      * default constructor
@@ -16,7 +19,7 @@ public class ReadWrite {
      * @return string array of all the words in the dictionary
      */
     public String[] readDictionary(String dictionaryFileName){
-        File f = new File(dictionaryFileName);
+        File f = new File(path + "/" + dictionaryFileName);
         Scanner fileScanner;
         ArrayList<String> dictList = new ArrayList<>();
 
@@ -38,6 +41,7 @@ public class ReadWrite {
     //main class for testing (temp)
     public static void main(String[] args) {
         ReadWrite rw = new ReadWrite();
+        //System.out.println(rw.path);
         System.out.println(rw.readDictionary("engDictionary.txt")[200]);
     }
 }
