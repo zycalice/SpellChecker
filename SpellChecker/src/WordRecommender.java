5ea0f37c67f41617359ed2804c9c0dc3a455b9f4
zycalice <alicezhang727@gmail.com>
2020-10-27 23:12:15 -0400

    added similarity calculation and unit test

--- SpellChecker/src/WordRecommender.java
+++ SpellChecker/src/WordRecommender.java
@@ -16,15 +16,17 @@ public class WordRecommender {
     }
 
     /**
-     * method to read in the dictionary
+     * method to read in the dictionary; make sure the file path exist
+     * make sure the dictionaryFileName input is in the right path; if error, check the current path
      * @return string array of all the words in the dictionary
      */    
     public String[] readDictionary(String dictionaryFileName){
-
+        //read dictionary as ArrayList first
         File f = new File (dictionaryFileName);
         Scanner fileScanner;
         ArrayList<String> dictList = new ArrayList<>();
 
+        //try reading the file; if file does not exist, print a msg and print the current path
         {
             try {
                 fileScanner = new Scanner(f);
@@ -32,10 +34,12 @@ public class WordRecommender {
                     dictList.add(fileScanner.nextLine());
                 }
             } catch (FileNotFoundException e) {
-                System.out.println("bad file name");
+                System.out.println("bad file name; current path is" + f.getAbsolutePath());
                 e.printStackTrace();
             }
         }
+
+        //convert to string array
         return dictList.toArray(String[]::new);
     }
 
@@ -54,6 +58,66 @@ public class WordRecommender {
     public String[] getDicWords(){
         return dictionaryWords;
     }
+
+    public int getWordLength(String word){
+        //empty word should have zero length
+        if (word.trim().equals("")){
+            return 0;
+        }
+        //otherwise, normal length
+        return word.split("").length;
+    }
+    /**
+     * get an string array for each character from the left
+     * @return string array for each character from the left
+     */
+    public String [] getWordLeftChars(String word){
+        return word.split("");
+    }
+
+    /**
+     * right characters
+     * @return string array of each character starting from the right
+     */
+    public String[] getWordRightChars(String word){
+        String [] rightChars = new String[getWordLength(word)];
+
+        //loop to get the reverse ordered characters
+        for (int i = 0; i < getWordLength(word); i++){
+            rightChars[i] = getWordLeftChars(word)[getWordLength(word)-i-1];
+        }
+        return rightChars;
+    }
+
+    /**
+     * get the number of same char and position
+     * @param wordArray1 string array of each characters in a word 1
+     * @param wordArray2 string array of each characters in a word 2
+     * @return an integer - the number of same char and position
+     */
+    public int getNumSameChars(String[] wordArray1, String[] wordArray2){
+        int numSame = 0;
+
+        //loop over the min of the characters to count similarity
+        for (int i = 0; i < Math.min(wordArray1.length, wordArray2.length);i++){
+            if (wordArray1[i].equals(wordArray2[i])){
+                numSame++;
+            }
+        }
+        return numSame;
+    }
+
+    /**
+     * return the avg similarity from left and right
+     * @param word1 one word
+     * @param word2 the second word
+     * @return a double - the avg similarity on two words
+     */
+    public double getSimilarity(String word1, String word2){
+        double leftSimilarity = getNumSameChars(getWordLeftChars(word1), getWordLeftChars(word2));
+        double rightSimilarity = getNumSameChars(getWordRightChars(word1), getWordRightChars(word2));
+        return  (leftSimilarity + rightSimilarity)/2;
+    }
     
     /*** 
     public ArrayList<String> getWordSuggestions (String word, int tolerance, double commonPercent, int topN){
@@ -65,8 +129,11 @@ public class WordRecommender {
         WordRecommender wr = new WordRecommender(path + "/" + "engDictionary.txt");
         String[] wr_DicW = wr.getDicWords();
         System.out.println(wr_DicW.length);
-
         System.out.println(new File("").getAbsolutePath());
+        System.out.println(wr.getSimilarity("haha","haha"));
+        System.out.println(wr.getSimilarity("hahe","haha"));
+        System.out.println(wr.getSimilarity("oblige","oblivion"));
+        System.out.println(wr.getSimilarity("aghast","gross"));
 
     }
 }
---
+++ SpellChecker/src/WordRecommenderTest.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordRecommenderTest {
+
+    WordRecommender wr;
+
+    @BeforeEach
+    void setUp() throws Exception{
+         wr = new WordRecommender("engDictionary.txt");
+    }
+
+    @Test
+    void testGetWordLength0(){
+        assertEquals(0, wr.getWordLength(" "));
+    }
+
+    @Test
+    void testGetWordLength1(){
+        assertEquals(1, wr.getWordLength("h"));
+    }
+
+    @Test
+    void testGetWordLength4(){
+        assertEquals(4, wr.getWordLength("haha"));
+    }
+
+    @Test
+    void testGetWordLeftChars(){
+        String [] expectation = {"h", "a", "h" ,"a"};
+        assertEquals(Arrays.toString(expectation), Arrays.toString(wr.getWordLeftChars("haha")));
+    }
+
+    @Test
+    void testGetWordRightChars(){
+        String [] expectation = {"a", "h", "a" ,"h"};
+        assertEquals(Arrays.toString(expectation), Arrays.toString(wr.getWordRightChars("haha")));
+    }
+
+    @Test
+    void testGetNumSameCharsLeft(){
+        String word1 = "oblige";
+        String word2 = "oblivion";
+
+        assertEquals(4, wr.getNumSameChars(wr.getWordLeftChars(word1),wr.getWordLeftChars(word2)));
+    }
+
+    @Test
+    void testGetNumSameCharsRight(){
+        String word1 = "oblige";
+        String word2 = "oblivion";
+
+        assertEquals(1, wr.getNumSameChars(wr.getWordRightChars(word1),wr.getWordRightChars(word2)));
+    }
+
+    @Test
+    void testGetSimilarity1(){
+        String word1 = "oblige";
+        String word2 = "oblivion";
+
+        assertEquals(2.5, wr.getSimilarity(word1,word2));
+    }
+
+    @Test
+    void testGetSimilarity2(){
+        String word1 = "aghast";
+        String word2 = "gross";
+
+        assertEquals(1.5, wr.getSimilarity(word1,word2));
+    }
+
+    @Test
+    void testGetSimilaritySameWord(){
+        String word1 = "haha";
+        String word2 = "haha";
+
+        assertEquals(4, wr.getSimilarity(word1,word2));
+    }
+    
+
+}
