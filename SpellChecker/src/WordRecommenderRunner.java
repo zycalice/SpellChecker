cab681c80b8efdcfbcec4436d786c7ad095ffea9
Viola Li <violali@Violas-MacBook-Air.local>
2020-11-04 21:56:57 -0500

    testfile

--- SpellChecker/src/WordRecommenderRunner.java
+++ SpellChecker/src/WordRecommenderRunner.java
@@ -8,7 +8,7 @@ public class WordRecommenderRunner {
 
     //main class to run
     public static void main(String[] args) {
-        String fileToCheck = "testFile1.txt";
+        String fileToCheck = "testFile2.txt";
         int dot = fileToCheck.indexOf(".");
         String fileToWrite = fileToCheck.substring(0, dot)+"_chk.txt";
         String dictToRead = "engDictionary.txt";
