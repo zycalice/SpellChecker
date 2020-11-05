53db5da51971890ce7333a3a7fe05245f6e41461
zycalice <alicezhang727@gmail.com>
2020-11-04 22:17:43 -0500

    removed unnecessary files, edited notes on file directory, and edited comments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Penn-CIT591-hw5
 Penn CIT 591 hw5 for collaboration: Spell Checker
+by: Xinyi li, Yuchen Zhang
+
+Please note that if you open the project from this folder, you need to put dictionary files in this folder to run the program. 
+It is better if you open the project from the SpellChecker folder, which already contains engDictionary and a couple other files.
+
--- SpellChecker/src/WordRecommenderRunner.java
+++ SpellChecker/src/WordRecommenderRunner.java
@@ -2,6 +2,12 @@
  * CIT 591 HW5, written by
  * Xinyi (Viola) Li
  * Yuchen Zhang
+ *
+ * Please note that if you open the project from on level above, you would need to
+ * put dictionary files in this folder to run the program.
+ * It is better if you open the project from the SpellChecker folder,
+ * which already contains engDictionary and a couple other files.
+ *
  */
 
 public class WordRecommenderRunner {
